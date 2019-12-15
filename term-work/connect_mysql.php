322ae9969bb0f819a50d9f525fce3f9eead260fe
st52952 <qomop@users.noreply.github.com>
2019-12-16 00:18:38 +0100

    +composer
    překopáno na neo4j

--- term-work/connect_mysql.php
+++ term-work/connect_mysql.php
@@ -1,6 +1,12 @@
 <?php
 
-$db = mysqli_connect('localhost','','','test');
+require_once 'vendor/autoload.php';
+
+use GraphAware\Neo4j\Client\ClientBuilder;
+
+$db = ClientBuilder::create()
+    ->addConnection('bolt','bolt://neo4j:pass@word1@localhost:7687')
+    ->build();
 
 if(!$db){
     die('error connecting to database');
